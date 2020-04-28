7331dfc05dd995c324077a76b2361f882560faaf
Ikraam <ikraamghoor@gmail.com>
2020-04-28 07:22:07 +0200

    Added latest answers and all files

---
+++ join.sql
@@ -0,0 +1,61 @@
+-- 1
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+-- 2
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012
+-- 3
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+
+-- 4
+SELECT team1,team2,player
+  FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
+-- 5
+SELECT player, teamid,coach, gtime
+  FROM goal JOIN eteam on teamid=id
+ WHERE gtime<=10
+-- 6
+SELECT mdate, teamname
+FROM game JOIN eteam ON (game.team1 = eteam.id)
+WHERE coach = 'Fernando Santos'
+-- 7
+SELECT player
+FROM game JOIN goal ON (id = matchid)
+WHERE stadium = 'National Stadium, Warsaw'
+-- 8
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE teamid != 'GER'
+    AND (team1 = 'GER' OR team2 = 'GER')
+-- 9
+SELECT teamname, COUNT(teamid)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname
+-- 10
+SELECT stadium, COUNT(player) 
+FROM game JOIN goal ON (id=matchid)
+GROUP BY stadium
+-- 11
+SELECT matchid,mdate, count(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid
+-- 12
+SELECT matchid, mdate, count(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'GER' OR team2 = 'GER')
+ AND teamid = 'GER'
+GROUP BY matchid
+-- 13 
+SELECT mdate,
+  team1,SUM(
+  CASE WHEN teamid=team1 THEN 1 ELSE 0 END)score1, team2,SUM(
+  CASE WHEN teamid=team2 THEN 1 ELSE 0 END)score2
+  FROM game LEFT JOIN goal ON id = matchid
+GROUP BY id
+ORDER BY mdate, matchid, team1, team2
+
---
+++ more_join.sql
@@ -0,0 +1,20 @@
+-- 1
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+
+-- 2
+
+-- 3
+
+-- 4
+
+-- 5
+
+-- 6
+
+-- 7
+
+-- 8
+
+-- 9
