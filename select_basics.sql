32cfee6c67e4d12118d98d0d67d14884d1f3afba
Ikraam <ikraamghoor@gmail.com>
2020-04-28 11:50:31 +0200

    Improved sytax format and finished null

--- select_basics.sql
+++ select_basics.sql
@@ -1,13 +1,21 @@
 -- 1
-SELECT population FROM world
-  WHERE name = 'Germany'
-
+SELECT
+  population
+FROM world
+WHERE
+  name = 'Germany'
 -- 2
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
-
+SELECT
+  name,
+  population
+FROM world
+WHERE
+  name IN ('Sweden', 'Norway', 'Denmark');
 -- 3
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
-
-  
+SELECT
+  name,
+  area
+FROM world
+WHERE
+  area BETWEEN 200000
+  AND 250000
--- select_in_select.sql
+++ select_in_select.sql
@@ -1,55 +1,107 @@
 -- 1
-SELECT name FROM world
-  WHERE population >
-     (SELECT population FROM world
-      WHERE name='Russia')
+SELECT
+  name
+FROM world
+WHERE
+  population > (
+    SELECT
+  population
+FROM world
+WHERE
+      name = 'Russia'
+  )
 -- 2
-SELECT name 
+SELECT
+  name
 FROM world
-WHERE continent = 'Europe' 
-AND gdp/population > 
- (SELECT gdp/population 
-   FROM world WHERE name = 'United Kingdom')
+WHERE
+  continent = 'Europe'
+  AND gdp / population > (
+    SELECT
+    gdp / population
+  FROM world
+  WHERE
+      name = 'United Kingdom'
+  )
 -- 3
-SELECT name, continent 
-  FROM world
-WHERE continent = ( 
-  SELECT continent 
-    FROM world 
-  WHERE name IN ('Argentina'))
-OR continent = ( 
-  SELECT continent 
-    FROM world 
-  WHERE name IN ('Australia'))
-ORDER BY name
--- 4
-SELECT name, population FROM world
-WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
--- 5
-SELECT name, CONCAT(ROUND(
-  (population/(SELECT population 
-   FROM world 
-     WHERE name = 
-    'Germany'
-    )*100
-    ),0
-    ), '%') 
+SELECT
+  name,
+  continent
 FROM world
-WHERE continent = 'Europe'
--- 6
-SELECT name 
+WHERE
+  continent = (
+    SELECT
+    continent
   FROM world
-WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
--- 7
-
--- 8
-
--- 9
-
--- 10
-
--- 11
-
--- 12
-
--- 13
+  WHERE
+      name IN ('Argentina')
+  )
+  OR continent = (
+    SELECT
+    continent
+  FROM world
+  WHERE
+      name IN ('Australia')
+  )
+ORDER BY
+  name
+-- 4
+SELECT
+  name,
+  population
+FROM world
+WHERE
+  population > (
+    SELECT
+    population
+  FROM world
+  WHERE
+      name = 'Canada'
+  )
+  AND population < (
+    SELECT
+    population
+  FROM world
+  WHERE
+      name = 'Poland'
+  )
+-- 5
+SELECT
+  name,
+  CONCAT(
+    ROUND(
+      (
+        population /(
+          SELECT
+    population
+  FROM world
+  WHERE
+            name = 'Germany'
+        ) * 100
+      ),
+      0
+    ),
+    '%'
+  )
+FROM world
+WHERE
+  continent = 'Europe'
+-- 6
+SELECT
+  name
+FROM world
+WHERE
+  gdp > ALL(
+    SELECT
+  gdp
+FROM world
+WHERE
+      continent = 'Europe'
+  AND gdp > 0
+  ) -- 7
+  -- 8
+  -- 9
+  -- 10
+  -- 11
+  -- 12
+  -- 13
