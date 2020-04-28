2244572334072c9bf1c400fe9b171eb2a436ec1f
Ikraam <ikraamghoor@gmail.com>
2020-04-28 14:04:48 +0200

    Foramatted code

--- self_join.sql
+++ self_join.sql
@@ -62,16 +62,18 @@ WHERE
   stopa.name = 'Craiglockhart'
   AND stopb.name = 'London Road'
 -- 7
-
 -- 8
-SELECT a.company, a.num
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart'
-  AND stopb.name = 'Tollcross'
-
--- 9
-
--- 10
+SELECT
+  a.company,
+  a.num
+FROM route a
+  JOIN route b ON (
+    a.company = b.company
+    AND a.num = b.num
+  )
+  JOIN stops stopa ON (a.stop = stopa.id)
+  JOIN stops stopb ON (b.stop = stopb.id)
+WHERE
+  stopa.name = 'Craiglockhart'
+  AND stopb.name = 'Tollcross' -- 9
+  -- 10
